064e8d426bbd82a0024562402fc27b58a58d21e7
chocolabo <hiddymeister@gmail.com>
2024-10-23 22:26:12 +0900

    ダークモードの修正

--- src/scripts/darkMode.ts
+++ src/scripts/darkMode.ts
@@ -3,18 +3,16 @@ interface Window {
   reverseDarkModeSetting: Function
   searchControlButton: Element | null
 }
+
 const scrollWidth =
-  document.body.scrollWidth || document.documentElement.scrollWidth
-let darkControlButton = null
-window.searchControlButton = null
-if (scrollWidth <= 742) {
-  darkControlButton = document.getElementsByClassName('darkwidget')[0]
-  window.searchControlButton = document.getElementsByClassName('searchwidget')[0]
-} else {
-  darkControlButton = document.getElementsByClassName('darknavbar')[0]
-  window.searchControlButton = document.getElementsByClassName('searchnavbar')[0]
-}
+  document.body.scrollWidth || document.documentElement.scrollWidth;
+
+let darkControlButton = null;
+window.searchControlButton = null;
+
+darkControlButton = document.getElementsByClassName('darknavbar')[0];
+window.searchControlButton = document.getElementsByClassName('searchnavbar')[0];
 
 darkControlButton.addEventListener('click', () => {
-  window.setDarkmode(window.reverseDarkModeSetting())
-})
+  window.setDarkmode(window.reverseDarkModeSetting());
+});
